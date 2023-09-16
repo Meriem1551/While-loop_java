567cde7b7ed93c2dba43ff9ef7a39459ff5768c3
Meriem1551 <meriem200409@gmail.com>
2023-09-16 18:25:17 +0200

    update project

--- src/main/java/org/math/Main.java
+++ src/main/java/org/math/Main.java
@@ -1,7 +1,15 @@
 package org.math;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        while (!input.equals("quit")){
+            System.out.print("Input: ");
+            input = scanner.next().toLowerCase();
+            System.out.println(input);
+        }
     }
 }
